e7b2ac771909455ed8a8c9df2186f61ff07350ab
jdhgit <17481018+jdhern17@users.noreply.github.com>
2021-11-04 10:56:51 -0600

    removed unnecessary server files

--- models/index.js
+++
@@ -1,6 +0,0 @@
-// because we are exporting multiple objects at once, define module.exports as the object with keys for referrer to reference
-// as index.js being the defaulted into file, export the other files as keys
-
-module.exports = {
-  Visitor: require("./visitor")
-};
