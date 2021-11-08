74d0b2b88e649dfe7ce336111701de15f55c8e9a
jdhgit <17481018+jdhern17@users.noreply.github.com>
2021-11-08 13:22:42 -0700

    cleaned server file by removing mongoose refs and express route refs. changed package.json scripts to always point to node server.js command

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 // define packages
 const express = require("express");
 const path = require("path");
-// const mongoose = require("mongoose");
-
-//pull in routes from routes folder (from index file directed to files within api folder)
-const routes = require("./routes");
 
 // instantiate express objects
 const app = express();
@@ -14,32 +10,20 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// pulls router.Router() from index of routes folder and applies to instantiated app object
-app.use(routes);
-
 // define behavior for serving assets in production
 // if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-//   });
-// }
-
-// else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.use(express.static("client/build"));
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 // }
 
-
-// connect to the mongo db
-// per mongo documentation: if the port number is not specified, the default port 27017 is used
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mr-dhzj", {
-//   useCreateIndex: true,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+// else {
+  // app.use(express.static(path.join(__dirname, '/client/public')));
+  // app.get("*", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  // });
+// }
 
 // start the express api server
 // callback function defined as the console.log that triggers given a successful definition of PORT variable
