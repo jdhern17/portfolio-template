aa278f98f0b99c1bc35f4cf415e12b3c1d2a73a6
jdhgit <17481018+jdhern17@users.noreply.github.com>
2021-11-30 18:58:52 -0700

    adding sendFile code to try and remove cannot get error

--- server.js
+++ server.js
@@ -1,64 +1,63 @@
 // define packages
 const express = require("express");
 const path = require("path");
-require('dotenv').config();
-const contentful = require('contentful');
+require("dotenv").config();
+const contentful = require("contentful");
 
 const SPACE_ID = process.env.SPACE_ID;
 const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
 
 const client = contentful.createClient({
-    space: SPACE_ID,
-    accessToken: ACCESS_TOKEN,
+  space: SPACE_ID,
+  accessToken: ACCESS_TOKEN,
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/client/build')));
-app.get("/content", function(req, res){
-  console.log("reached /content route");
-  client.getEntries().then(response => {
-    console.log(response);
-    res.json(response.items)
-  }).catch(err => {
-    res.status(500).json(err);
-  })
-})
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("/content", function (req, res) {
+  client
+    .getEntries()
+    .then((response) => {
+      res.json(response.items);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 // app.get("*", function(req, res) {
 //   res.sendFile(path.join(__dirname, "./client/public/index.html"));
 // });
+// if (process.env.NODE_ENV === "production") {
+// app.use(express.static("client/build"));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+// }
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
 
-// if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("client/build"));
-  // app.get("*", function(req, res) {
-    //   console.log("another check from within wildcard route");
-    //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    // });
-    // }
-    
-    // else {
-      // app.use(express.static(path.join(__dirname, '/client/public')));
-      
-      // }
-      
-      //   try {
-      //     const response = await client.getEntries();
-      //     return {
-      //         headers,
-      //         statusCode: 200,
-      //         body: JSON.stringify(response.items, null, 2),
-      //     };
-      // } catch (ex) {
-      //     return {
-      //         headers,
-      //         statusCode: 500,
-      //         body: ex.message,
-      //     };
-      // }
+// else {
+// app.use(express.static(path.join(__dirname, '/client/public')));
+
+// }
+
+//   try {
+//     const response = await client.getEntries();
+//     return {
+//         headers,
+//         statusCode: 200,
+//         body: JSON.stringify(response.items, null, 2),
+//     };
+// } catch (ex) {
+//     return {
+//         headers,
+//         statusCode: 500,
+//         body: ex.message,
+//     };
+// }
